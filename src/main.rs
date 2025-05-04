1909b924b34181ebf12abfd45ac0a1bfd7c2770f
Kross1de <awesome.hubygu@yandex.ru>
2025-05-04 19:59:14 +0600

    Almost completed vga buffer

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,16 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World!";
+mod vga_buffer;
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is entry point, since the linker looks for a function
     // name '_start' by default
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
 
     loop {}
 }
