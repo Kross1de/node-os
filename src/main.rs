5ebcf258b502c882b70d4ec6c1271805cd0b1aba
Kross1de <awesome.hubygu@yandex.ru>
2025-05-05 10:22:54 +0600

    Start working with paging

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,10 @@ pub extern "C" fn _start() -> ! {
 
     NodeOS::init();
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
-
-    // uncomment line below to trigger a stack overflow
-    // stack_overflow();
+	use x86_64::registers::control::Cr3;
+	
+	let (level_4_page_table, _) = Cr3::read();
+	println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
     
     #[cfg(test)]
     test_main();
